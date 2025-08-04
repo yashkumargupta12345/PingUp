9e0da22e4bc89765be0fce53d86875735725f1bc
yashkumargupta12345 <guptayashyg45227@gmail.com>
2025-08-04 15:21:02 +0530

    First Commit and added the routes for all the pages

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Feed from './pages/Feed'
+import Messages from './pages/Messages'
+import ChatBox from './pages/ChatBox'
+import Connections from './pages/Connections'
+import Discover from './pages/Discover'
+import Profile from './pages/Profile'
+import CreatePost from './pages/CreatePost'
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Login />}>
+          <Route index element={<Feed />} />
+          <Route path='messages' element={<Messages />} />
+          <Route path='messages/:userId' element={<ChatBox />} />
+          <Route path='connections' element={<Connections />} />
+          <Route path='discover' element={<Discover />} />
+          <Route path='profile' element={<Profile />} />
+          <Route path='profile/:id' element={<Profile />} />
+          <Route path='create-post' element={<CreatePost />} />
+           
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
