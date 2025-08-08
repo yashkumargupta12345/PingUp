8f6c602c72226f29282c620564d669312a0e18f1
yashkumargupta12345 <guptayashyg45227@gmail.com>
2025-08-08 17:05:22 +0530

    Added the Sidebar and also Created the Story section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,12 +8,19 @@ import Connections from './pages/Connections'
 import Discover from './pages/Discover'
 import Profile from './pages/Profile'
 import CreatePost from './pages/CreatePost'
+import { useUser } from '@clerk/clerk-react'
+import Layout from './pages/Layout'
+import {Toaster} from 'react-hot-toast'
+
 
 const App = () => {
+  const { user } = useUser()
+
   return (
     <>
+      <Toaster />
       <Routes>
-        <Route path='/' element={<Login />}>
+        <Route path='/' element={!user ? <Login /> : <Layout />}>
           <Route index element={<Feed />} />
           <Route path='messages' element={<Messages />} />
           <Route path='messages/:userId' element={<ChatBox />} />
--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyPostsData } from '../assets/assets'
+import Loading from '../components/Loading'
+import StoriesBar from '../components/StoriesBar'
 
 const Feed = () => {
-  return (
-    <div>
-      
+
+  const [feeds, setFeeds] = useState([])
+  const [loading, setLoading] = useState(true)
+
+
+  const fetchFeeds = async () => {
+    setFeeds(dummyPostsData)
+    setLoading(false)
+  }
+
+  useEffect(()=> {
+    fetchFeeds()
+  }, [])
+
+
+  return !loading ? (
+    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+      {/* Stories and post list  */}
+      <div>
+        <StoriesBar />
+        <div className='p-4 space-y-6'>
+          List of post
+        </div>
+      </div>
+
+      {/* Right sidebar  */}
+      <div>
+        <div>
+          <h1>Sponsored</h1>
+        </div>
+        <h1>Recent messages</h1>
+      </div>
     </div>
-  )
+  ) : <Loading />
 }
 
 export default Feed
