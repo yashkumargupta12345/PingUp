13c59c963939b538a2232358e88baa1ded3de7c8
yashkumargupta12345 <guptayashyg45227@gmail.com>
2025-08-09 19:14:45 +0530

    Added the UI for the Feed section and the Sponsor and recent messages section

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { dummyPostsData } from '../assets/assets'
+import { assets, dummyPostsData } from '../assets/assets'
 import Loading from '../components/Loading'
 import StoriesBar from '../components/StoriesBar'
+import PostCard from '../components/PostCard'
+import RecentMessages from '../components/RecentMessages'
 
 const Feed = () => {
 
@@ -25,16 +27,21 @@ const Feed = () => {
       <div>
         <StoriesBar />
         <div className='p-4 space-y-6'>
-          List of post
+          {feeds.map((post)=>(
+            <PostCard key={post._id} post={post}/>
+            ))}
         </div>
       </div>
 
       {/* Right sidebar  */}
-      <div>
-        <div>
-          <h1>Sponsored</h1>
+      <div className='max-xl:hidden sticky top-0'>
+        <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
+          <h3 className='text-slate-800 font-semibold'>Sponsored</h3>
+          <img src={assets.sponsored_img} className='w-75 h-50 rounded-md' alt="" />
+          <p className='text-slate-600'>Email Marketing</p>
+          <p className='text-slate-400'>Supercharge your marketing with a powerful, easy-to-use platform build for results.</p>
         </div>
-        <h1>Recent messages</h1>
+        <RecentMessages />
       </div>
     </div>
   ) : <Loading />
